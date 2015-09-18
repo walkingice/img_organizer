c6728789b162eaa8a46d474bbbea60536b725b9e
Julian_Chu <walkingice@0xlab.org>
2015-09-18 12:50:12 +0800

    change activity name in activiy
    Fragment.onAttach(Activity) was deprecated. use onAttachFragment in
    Activity.
    
    and I like zero-base index.

--- app/src/main/java/org/zeroxlab/imgorg/MainActivity.java
+++ app/src/main/java/org/zeroxlab/imgorg/MainActivity.java
@@ -47,26 +47,32 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    public void onAttachFragment(Fragment fragment) {
+        Bundle bundle = fragment.getArguments();
+        if (bundle == null) { // in initialization
+            return;
+        }
+        int number = bundle.getInt(PlaceholderFragment.ARG_SECTION_NUMBER, 0);
+        switch (number) {
+            case 0:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 1:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+
+    @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, PlaceholderFragment.newInstance(position))
                 .commit();
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
     }
 
     public void restoreActionBar() {
@@ -135,13 +141,6 @@ public class MainActivity extends AppCompatActivity
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
 
